6ee568a3183e553d18d4f92cb554338f4f0829df
Alberto <albertojunior848423803@gmail.com>
2025-05-24 21:22:27 +0300

    changes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -23,6 +23,7 @@ import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+
 const MotionBox = motion(Box);
 const MotionButton = motion(Button);
 
